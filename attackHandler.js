9036f21baf5bb2eaa0bdeff5c81e7b4673a31afb
Magenz <56995698+Magenz@users.noreply.github.com>
2020-11-13 09:43:54 +0800

    Add files via upload
    Added redirect to sell at full inventory

--- attackHandler.js
+++ attackHandler.js
@@ -1,2 +1,14 @@
 var atk = document.querySelector('input[value*="ATTACK"]');
-atk.click();
+
+
+var myEles = document.querySelectorAll('strong');
+for(var i=0; i<myEles.length; i++){
+    
+    if(myEles[i].innerHTML == 'No space in your inventory.'){
+        window.location.href = "https://blackdragon.mobi/buildings/view/building=calh/action=sell/c=14749?submit=SELL";
+        
+    }
+    else{
+     atk.click();  
+    }
+}
