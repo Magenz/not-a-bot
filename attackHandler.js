402886124e3a1cd998b109337f677b12832e4a0f
Magenz <56995698+Magenz@users.noreply.github.com>
2020-11-17 07:00:05 +0800

    Add files via upload

--- attackHandler.js
+++ attackHandler.js
@@ -5,7 +5,7 @@ var myEles = document.querySelectorAll('strong');
 for(var i=0; i<myEles.length; i++){
     
     if(myEles[i].innerHTML == 'No space in your inventory.'){
-        window.location.href = "https://blackdragon.mobi/buildings/view/building=calh/action=sell/c=14749?submit=SELL";
+        window.location.href = "https://blackdragon.mobi/buildings/view/building=calh/action=sell/all=1/c=";
         
     }
     else{
